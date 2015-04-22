4cfb9a7f9bb4c64259fcfd167cf5b9d525056fcc
LuisTrrs <luis.torres.ortiz>
2015-04-22 10:39:33 -0500

    Se agregan estilos nuevos

--- index.php
+++ index.php
@@ -22,19 +22,7 @@
                 <input type="reset" id="login" value="Reset" />
             </div>
         </form><!-- form -->
-        <form action="" method="post" class="login">
-            <h1>Registrate</h1>
-            <div>
-                <input name="user" type="text" placeholder="Username" id="username" pattern="^[A-Za-zñ,í,á,é,ó,ú,Á,É,Í,Ó,Ú,Ñ]{8}$" title="El Usuario debe de contener 8 caracteres." required />
-            </div>
-            <div>
-                <input type="password" name="password" type="password" placeholder="Password" id="password" pattern="^[A-Za-zñ,í,á,é,ó,ú,Á,É,Í,Ó,Ú,Ñ]{6}[0-9]{2}$" title="La Contrase&ntilde;a debe contener 6 caracteres y al final 2 N&uacute;meros. Ej: contra22" required />
-            </div>
-            <div class="div2s">
-                <input name="login" type="submit" value="login" />
-                <input type="reset" id="login" value="Reset" />
-            </div>
-        </form><!-- form -->
+        
     </section><!-- content -->
 </div><!-- container -->
 
